c02c43aa0fbd717144cb2ab440b2ca55649a151f
Elena-Weber <crochetmiracles@gmail.com>
2022-08-28 20:06:57 -0400

    add drawer and fix bug

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { Text, Button, StyleSheet } from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack'; // it's an object with 2 obligatory components: Navigator and Screen
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import Animated from 'react-native-reanimated';
 import CategoriesScreen from './screens/CategoriesScreen';
 import MealsScreen from './screens/MealsScreen';
 import MealDetails from './screens/MealDetails';
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function drawerNavigator() {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="first" />
+      <Drawer.Screen name="second" />
+    </Drawer.Navigator>
+  );
+};
 
 export default function App() {
   return (
@@ -16,7 +29,7 @@ export default function App() {
         <Stack.Navigator
         // applies to all screens by default
           screenOptions={{
-              headerStyle: {backgroundColor: '#005236'},
+              headerStyle: {backgroundColor: '#20686c'},
               headerTintColor: 'white',
               contentStyle: {backgroundColor: 'white'}
             }}
